d4654fe5907b0608a4f1221a6ee080860d5f6d0b
unknown <you@example.com>
2026-04-18 20:28:34 +0600

    Add hero banner in cources section

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import Card from "../Components/Card";
 
 const Courses = () => {
-  return <div>Courses Page</div>;
+  return (
+    <div className="max-w-11/12 mx-auto">
+      <img
+        className="w-full h-[250px] md:h-[400px] object-cover rounded-lg"
+        src="https://img.freepik.com/free-psd/black-friday-super-sale-facebook-cover-banner-template_120329-5177.jpg?semt=ais_hybrid&w=740&q=80"
+        alt=""
+      />
+      <div className="max-w-full md:max-w-3xl lg:max-w-4xl ">
+        <h2 className="text-3xl md:text-lg font-bold text-gray-800 my-4">
+          Courses
+        </h2>
+
+        <p className="text-gray-600 leading-relaxed">
+          We offer all the trendy courses that are in demand in the global
+          market. In addition, you are getting lab facilities where high-end
+          computers with the required configuration are ready to facilitate your
+          learning. After class, you can practice the topic in our labs to grow
+          your skills. The courses are designed to make you confident throughout
+          the learning journey with Creative IT.
+        </p>
+      </div>
+
+      <div></div>
+    </div>
+  );
 };
 
 export default Courses;
